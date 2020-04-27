cea876e84e01ee6e4ac6660f5126a2032742d43a
JayJayDee <jindongp@gmail.com>
2020-04-27 23:52:52 +0900

    removed dark mode. added about page.

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card>
+        <v-card-text>
+          <p>Dr.Choice</p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
